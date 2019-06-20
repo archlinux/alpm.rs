5c060551f71e205188ff4c779a41a2739d6a552a
morganamilo <morganamilo@gmail.com>
2019-06-20 01:14:24 +0100

    Add benchmark

---
+++ alpm/benches/list.rs
@@ -0,0 +1,15 @@
+use alpm::{Alpm, SigLevel};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn benchmark_list(c: &mut Criterion) {
+    c.bench_function("list", |b| {
+        let handle = Alpm::new("/", "tests/db").unwrap();
+        let db = handle.register_syncdb("core", SigLevel::NONE).unwrap();
+        let pkg = db.pkg("linux").unwrap();
+
+        b.iter(|| { black_box(&pkg.depends().collect::<Vec<_>>()); });
+    });
+}
+
+criterion_group!(benches, benchmark_list);
+criterion_main!(benches);
