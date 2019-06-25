d0b81a4596e83a4e19eb7b0054986c0cb3844ad0
morganamilo <morganamilo@gmail.com>
2019-06-25 20:16:22 +0100

    Add alpm-utils

---
+++ alpm-utils/src/depends.rs
@@ -0,0 +1,52 @@
+use alpm::{Depend, DepMod, vercmp};
+
+use std::cmp::Ordering;
+
+/// Checks if a dependency is satisfied by a package (name + version).
+pub fn satisfies_dep<'a, D: Into<Depend<'a>>, S: AsRef<str>>(dep: D, name: S, version: S) -> bool {
+    let dep = dep.into();
+    let name = name.as_ref();
+    let version = version.as_ref();
+
+    if dep.name() != name {
+        return false;
+    }
+
+    satisfies_ver(dep, version)
+}
+
+/// Checks if a dependency is satisdied by a provide.
+pub fn satisfies_provide<'a, D: Into<Depend<'a>>>(dep: D, provide: D) -> bool {
+    let dep = dep.into();
+    let provide = provide.into();
+
+    if dep.name() != provide.name() {
+        return false;
+    }
+
+    if provide.depmod() == DepMod::Any && dep.depmod() != DepMod::Any {
+        return false;
+    }
+
+    satisfies_ver(dep, provide.version())
+}
+
+fn satisfies_ver<'a, D: Into<Depend<'a>>, S: AsRef<str>>(dep: D, version: S) -> bool {
+    let dep = dep.into();
+    let version = version.as_ref();
+
+    if dep.depmod() == DepMod::Any {
+        return true;
+    }
+
+    let cmp = vercmp(dep.version(), version);
+
+    match dep.depmod() {
+        DepMod::Eq => cmp == Ordering::Equal,
+        DepMod::Ge => cmp == Ordering::Greater || cmp == Ordering::Equal,
+        DepMod::Le => cmp == Ordering::Less || cmp == Ordering::Equal,
+        DepMod::Gt => cmp == Ordering::Greater,
+        DepMod::Lt => cmp == Ordering::Less,
+        DepMod::Any => true,
+    }
+}
---
+++ alpm-utils/src/lib.rs
@@ -0,0 +1,8 @@
+//! #alpm-utils
+//!
+//! A utility libary that provides some common functionality an alpm user may requre.
+
+#![warn(missing_docs)]
+
+/// Utils for dependency checking.
+pub mod depends;
