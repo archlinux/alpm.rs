f10d8277fd48ac663f9428d9d3e16cf4aff99163
morganamilo <morganamilo@gmail.com>
2019-06-25 18:54:57 +0100

    Return Ordering for vercmp

--- alpm/src/version.rs
+++ alpm/src/version.rs
@@ -1,32 +1,18 @@
-use crate::Result;
-
+use std::cmp::Ordering;
 use std::ffi::CString;
-use std::os::raw::c_int;
 
 use alpm_sys::*;
 
-pub fn vercmp<S: Into<String>>(a: S, b: S) -> Result<Vercmp> {
+pub fn vercmp<S: Into<String>>(a: S, b: S) -> Ordering {
     let a = CString::new(a.into()).unwrap();
     let b = CString::new(b.into()).unwrap();
     let ret = unsafe { alpm_pkg_vercmp(a.as_ptr(), b.as_ptr()) };
-    Ok(ret.into())
-}
 
-#[derive(Debug, Eq, PartialEq, Copy, Clone, Ord, PartialOrd, Hash)]
-pub enum Vercmp {
-    Older,
-    Equal,
-    Newer,
-}
-
-impl From<c_int> for Vercmp {
-    fn from(int: c_int) -> Vercmp {
-        if int < 0 {
-            Vercmp::Older
-        } else if int > 0 {
-            Vercmp::Newer
-        } else {
-            Vercmp::Equal
-        }
+    if ret < 0 {
+        Ordering::Less
+    } else if ret > 0 {
+        Ordering::Greater
+    } else {
+        Ordering::Greater
     }
 }
