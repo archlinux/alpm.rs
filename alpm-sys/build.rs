8c6cda3a990cc721cc1f599da48c0dd7226d8c60
morganamilo <morganamilo@archlinux.org>
2020-10-13 00:03:06 +0100

    Add ability to generate bindings at build time

--- alpm-sys/build.rs
+++ alpm-sys/build.rs
@@ -1,3 +1,35 @@
 fn main() {
     println!("cargo:rustc-link-lib=alpm");
+
+    #[cfg(feature = "generate")]
+    {
+        use std::env;
+        use std::path::Path;
+
+        let out_dir = env::var_os("OUT_DIR").unwrap();
+        let dest_path = Path::new(&out_dir).join("ffi_generated.rs");
+
+        let bindings = bindgen::builder()
+            .header("/usr/include/alpm.h")
+            .whitelist_type("(alpm|ALPM).*")
+            .whitelist_function("(alpm|ALPM).*")
+            .rustified_enum("_alpm_[a-z_]+_t")
+            .constified_enum_module("_alpm_siglevel_t")
+            .constified_enum_module("_alpm_pkgvalidation_t")
+            .constified_enum_module("_alpm_loglevel_t")
+            .constified_enum_module("_alpm_question_type_t")
+            .constified_enum_module("_alpm_transflag_t")
+            .constified_enum_module("_alpm_db_usage_")
+            .constified_enum_module("_alpm_db_usage_t")
+            .constified_enum_module("alpm_caps")
+            .opaque_type("alpm_handle_t")
+            .opaque_type("alpm_db_t")
+            .opaque_type("alpm_pkg_t")
+            .opaque_type("alpm_trans_t")
+            .size_t_is_usize(true)
+            .generate()
+            .unwrap();
+
+        bindings.write_to_file(dest_path).unwrap();
+    }
 }
