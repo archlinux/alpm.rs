d85ac5408c3c37ee21c9ef05f73d57a7449a2a26
morganamilo <morganamilo@archlinux.org>
2021-04-21 00:52:01 +0100

    Tweak version mismatch message

---
+++ alpm/build.rs
@@ -0,0 +1,23 @@
+use alpm_sys::alpm_version;
+use std::ffi::CStr;
+
+fn main() {
+    if cfg!(feature = "checkver") && !cfg!(feature = "git") {
+        let ver = unsafe { alpm_version() };
+        assert!(!ver.is_null());
+        let ver = unsafe { CStr::from_ptr(ver) };
+        let ver = ver.to_str().unwrap();
+
+        let parts = ver.split('.').collect::<Vec<_>>();
+        let parts = parts
+            .iter()
+            .map(|v| v.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
+
+        assert!(
+            parts[0] == 12 && parts[2] <= 2,
+            "this version of alpm.rs does not support libalpm v{} only v12.x.0 - v12.x.2 is supported",
+            ver,
+        );
+    }
+}
