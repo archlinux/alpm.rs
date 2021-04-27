fe5288ce420a9697679b47656ffd5db1e30121ce
morganamilo <morganamilo@archlinux.org>
2021-04-27 07:53:04 +0100

    Update db update

--- alpm/src/be_sync.rs
+++ alpm/src/be_sync.rs
@@ -1,25 +1,8 @@
 use crate::Result;
 use alpm_sys::*;
 
-use crate::DbMut;
+use crate::{AlpmList, DbMut};
 
-#[cfg(feature = "git")]
-use crate::AlpmList;
-
-#[cfg(not(feature = "git"))]
-impl<'a> DbMut<'a> {
-    pub fn update(&mut self, force: bool) -> Result<bool> {
-        let force = if force { 1 } else { 0 };
-        let ret = unsafe { alpm_db_update(force, self.db) };
-        if ret < 0 {
-            Err(self.handle.last_error())
-        } else {
-            Ok(ret == 1)
-        }
-    }
-}
-
-#[cfg(feature = "git")]
 impl<'a> AlpmList<'a, DbMut<'a>> {
     pub fn update(&self, force: bool) -> Result<bool> {
         let force = if force { 1 } else { 0 };
