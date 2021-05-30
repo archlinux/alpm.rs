d6feab3bbe0d98d80b47cee6130966bbec978e74
morganamilo <morganamilo@archlinux.org>
2021-05-30 17:56:29 +0100

    Don't check ptr for null when creating cstr

--- alpm/src/utils.rs
+++ alpm/src/utils.rs
@@ -2,9 +2,7 @@ use std::ffi::CStr;
 use std::os::raw::c_char;
 
 pub unsafe fn from_cstr<'a>(s: *const c_char) -> &'a str {
-    s.as_ref()
-        .map(|s| CStr::from_ptr(s).to_str().unwrap())
-        .unwrap()
+    CStr::from_ptr(s).to_str().unwrap()
 }
 
 pub unsafe fn from_cstr_optional<'a>(s: *const c_char) -> Option<&'a str> {
