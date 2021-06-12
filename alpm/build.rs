13d9b40dc4241ef67ec8b7440ff148cd3ce2977b
morganamilo <morganamilo@archlinux.org>
2021-06-12 20:11:47 +0100

    Simplyfy build script
    This also lets git builds run in CI as the build script never pulls
    in alpm.

--- alpm/build.rs
+++ alpm/build.rs
@@ -1,37 +1,37 @@
-#[cfg(feature = "checkver")]
 fn main() {
-    use alpm_sys::alpm_version;
-    use std::ffi::CStr;
+    #[cfg(feature = "checkver")]
+    {
+        #[cfg(not(feature = "git"))]
+        {
+            use alpm_sys::alpm_version;
+            use std::ffi::CStr;
 
-    if cfg!(feature = "checkver") && !cfg!(feature = "git") {
-        let ver = unsafe { alpm_version() };
-        assert!(!ver.is_null());
-        let ver = unsafe { CStr::from_ptr(ver) };
-        let ver = ver.to_str().unwrap();
+            let ver = unsafe { alpm_version() };
+            assert!(!ver.is_null());
+            let ver = unsafe { CStr::from_ptr(ver) };
+            let ver = ver.to_str().unwrap();
 
-        let parts = ver.split('.').collect::<Vec<_>>();
-        let parts = parts
-            .iter()
-            .map(|v| v.parse::<i32>().unwrap())
-            .collect::<Vec<_>>();
+            let parts = ver.split('.').collect::<Vec<_>>();
+            let parts = parts
+                .iter()
+                .map(|v| v.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
 
-        let current = parts[0];
-        let revision = parts[1];
-        let age = parts[2];
+            let current = parts[0];
+            let revision = parts[1];
+            let age = parts[2];
 
-        let supported_current = 13;
-        let supported_revision = 0;
+            let supported_current = 13;
+            let supported_revision = 0;
 
-        assert!(
-            supported_current == current
-                && (revision - age..=revision).contains(&supported_revision),
-            "this version of alpm.rs does not support libalpm v{} only v{}.{}.0 is supported",
-            ver,
-            supported_current,
-            supported_revision,
-        );
+            assert!(
+                supported_current == current
+                    && (revision - age..=revision).contains(&supported_revision),
+                "this version of alpm.rs does not support libalpm v{} only v{}.{}.0 is supported",
+                ver,
+                supported_current,
+                supported_revision,
+            );
+        }
     }
 }
-
-#[cfg(not(feature = "checkver"))]
-fn main() {}
