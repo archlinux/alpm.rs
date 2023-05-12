cf070a15c186063951ed23e92c4d49839c416d00
Colin Woodbury <colin@fosskers.ca>
2023-05-12 10:28:30 +0900

    feat(utils): revive `AlpmExt` yet again
    This required implementing some custom iterator structs, since `impl
    Foo` is not allowed for the return types of trait methods.

--- alpm-utils/src/alpm.rs
+++ alpm-utils/src/alpm.rs
@@ -1,24 +1,74 @@
 //! Extension methods for the [`Alpm`] type.
 
 use crate::DbListExt;
-use alpm::{Alpm, Package};
+use alpm::{Alpm, AlpmList, Db, IntoIter, Package};
 
-/// All official packages.
-pub fn native_packages(alpm: &Alpm) -> impl Iterator<Item = Package<'_>> {
-    let syncs = alpm.syncdbs();
+/// Extension methods for [`Alpm`] which aren't critical enough to live in the
+/// main crate, directly within `Alpm`.
+pub trait AlpmExt {
+    /// An iterator of [`Package`]s that are found in "sync databases",
+    /// typically registered in one's `pacman.conf`.
+    fn native_packages<'a>(&'a self) -> NativePkgs<'a>;
 
-    alpm.localdb()
-        .pkgs()
-        .into_iter()
-        .filter_map(move |p| syncs.pkg(p.name()).ok())
+    /// The opposite of [`AlpmExt::native_packages`]; installed packages that
+    /// aren't found in any registered "sync database".
+    fn foreign_packages<'a>(&'a self) -> ForeignPkgs<'a>;
 }
 
-/// All foreign packages as an `Iterator`.
-pub fn foreign_packages(alpm: &Alpm) -> impl Iterator<Item = Package<'_>> {
-    let syncs = alpm.syncdbs();
+impl AlpmExt for Alpm {
+    fn native_packages<'a>(&'a self) -> NativePkgs<'a> {
+        NativePkgs::new(self)
+    }
 
-    alpm.localdb()
-        .pkgs()
-        .into_iter()
-        .filter(move |p| syncs.pkg(p.name()).is_err())
+    fn foreign_packages<'a>(&'a self) -> ForeignPkgs<'a> {
+        ForeignPkgs::new(self)
+    }
+}
+
+/// [`Package`]s that are found in registered "sync databases".
+pub struct NativePkgs<'a> {
+    local: IntoIter<'a, Package<'a>>,
+    sync: AlpmList<'a, Db<'a>>,
+}
+
+impl<'a> NativePkgs<'a> {
+    fn new(alpm: &'a Alpm) -> NativePkgs<'a> {
+        let local = alpm.localdb().pkgs().into_iter();
+        let sync = alpm.syncdbs();
+
+        NativePkgs { local, sync }
+    }
+}
+
+impl<'a> Iterator for NativePkgs<'a> {
+    type Item = Package<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let s = self.sync;
+        self.local.find(|p| s.pkg(p.name()).is_ok())
+    }
+}
+
+/// Installed [`Package`]s that are _not_ found in registered "sync databases".
+pub struct ForeignPkgs<'a> {
+    local: IntoIter<'a, Package<'a>>,
+    sync: AlpmList<'a, Db<'a>>,
+}
+
+impl<'a> ForeignPkgs<'a> {
+    fn new(alpm: &'a Alpm) -> ForeignPkgs<'a> {
+        let local = alpm.localdb().pkgs().into_iter();
+        let sync = alpm.syncdbs();
+
+        ForeignPkgs { local, sync }
+    }
+}
+
+impl<'a> Iterator for ForeignPkgs<'a> {
+    type Item = Package<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let s = self.sync;
+        self.local.find(|p| s.pkg(p.name()).is_err())
+    }
 }
