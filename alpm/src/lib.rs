188615fb3b5880890b14b8b24c6289d7f3ab65ab
Arvid Norlander <VorpalBlade@users.noreply.github.com>
2024-02-10 20:38:38 +0100

    The unused imports at the top level are intentional, quieten warnings

--- alpm/src/lib.rs
+++ alpm/src/lib.rs
@@ -34,23 +34,29 @@ mod version;
 
 pub use crate::add::*;
 pub use crate::alpm::*;
+#[allow(unused_imports)]
 pub use crate::be_local::*;
 pub use crate::be_pkg::*;
+#[allow(unused_imports)]
 pub use crate::be_sync::*;
 pub use crate::cb::*;
 pub use crate::conflict::*;
 pub use crate::db::*;
 pub use crate::deps::*;
+#[allow(unused_imports)]
 pub use crate::dload::*;
 pub use crate::error::*;
 pub use crate::filelist::*;
+#[allow(unused_imports)]
 pub use crate::handle::*;
 pub use crate::list::*;
 #[cfg(feature = "mtree")]
 pub use crate::mtree::*;
 pub use crate::package::*;
+#[allow(unused_imports)]
 pub use crate::remove::*;
 pub use crate::signing::*;
+#[allow(unused_imports)]
 pub use crate::sync::*;
 pub use crate::trans::*;
 pub use crate::types::*;
