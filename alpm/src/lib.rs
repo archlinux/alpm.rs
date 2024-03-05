ebb266103e96e86ccdd7f04b6780eedb60efcf10
morganamilo <morganamilo@archlinux.org>
2024-03-05 20:58:37 +0000

    Remove unused code

--- alpm/src/lib.rs
+++ alpm/src/lib.rs
@@ -36,26 +36,20 @@ mod version;
 
 pub use crate::add::*;
 pub use crate::alpm::*;
-pub use crate::be_local::*;
 pub use crate::be_pkg::*;
-pub use crate::be_sync::*;
 pub use crate::cb::*;
 pub use crate::conflict::*;
 pub use crate::db::*;
 pub use crate::deps::*;
-pub use crate::dload::*;
 pub use crate::error::*;
 pub use crate::filelist::*;
-pub use crate::handle::*;
 pub use crate::list::*;
 pub use crate::list_mut::*;
 pub use crate::list_with::*;
 #[cfg(feature = "mtree")]
 pub use crate::mtree::*;
 pub use crate::package::*;
-pub use crate::remove::*;
 pub use crate::signing::*;
-pub use crate::sync::*;
 pub use crate::trans::*;
 pub use crate::types::*;
 pub use crate::unions::*;
