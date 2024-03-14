9c24713d8e68d0cb38784f04d81b0ba2f575cd3f
Xavier <sapphirus@azorium.net>
2024-03-14 11:38:53 -0400

    Alter function signature to accept borrowed &Package objects

--- alpm/src/remove.rs
+++ alpm/src/remove.rs
@@ -3,7 +3,7 @@ use crate::{Alpm, Package, Result};
 use alpm_sys::*;
 
 impl Alpm {
-    pub fn trans_remove_pkg(&self, pkg: Package) -> Result<()> {
+    pub fn trans_remove_pkg(&self, pkg: &Package) -> Result<()> {
         let ret = unsafe { alpm_remove_pkg(self.as_ptr(), pkg.as_ptr()) };
         self.check_ret(ret)
     }
